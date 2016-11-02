5ec3844c65c83f35a79e5cfd0aa40b941837cc35
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-11-02 19:40:24 +0700

    [REFACTOR] Signin and Signup Activity, and also layout - Modified Signin using ConstraintLayout - Modified strings.xml - Etc

--- app/src/main/java/com/weouts/program/andro/weouts/SigninActivity.java
+++ app/src/main/java/com/weouts/program/andro/weouts/SigninActivity.java
@@ -20,12 +20,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity{
+public class SigninActivity extends AppCompatActivity {
     private EditText inputEmail, inputPassword;
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private ProgressBar progressBar;
-    private Button btnSignup, btnLogin, btnReset;
+    private Button btnLogin, btnReset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,35 +35,27 @@ public class LoginActivity extends AppCompatActivity{
         auth = FirebaseAuth.getInstance();
 
         if (auth.getCurrentUser() != null) {
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            startActivity(new Intent(SigninActivity.this, MainActivity.class));
             finish();
         }
 
         // set the view now
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signin);
 
 
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnSignup = (Button) findViewById(R.id.btn_signup);
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnReset = (Button) findViewById(R.id.btn_reset_password);
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
 
-        btnSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
-            }
-        });
-
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+                startActivity(new Intent(SigninActivity.this, ResetPasswordActivity.class));
             }
         });
 
@@ -86,7 +79,7 @@ public class LoginActivity extends AppCompatActivity{
 
                 //authenticate user
                 auth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        .addOnCompleteListener(SigninActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 // If sign in fails, display a message to the user. If sign in succeeds
@@ -98,10 +91,10 @@ public class LoginActivity extends AppCompatActivity{
                                     if (password.length() < 6) {
                                         inputPassword.setError(getString(R.string.minimum_password));
                                     } else {
-                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                        Toast.makeText(SigninActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
                                     }
                                 } else {
-                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    Intent intent = new Intent(SigninActivity.this, MainActivity.class);
                                     startActivity(intent);
                                     finish();
                                 }
