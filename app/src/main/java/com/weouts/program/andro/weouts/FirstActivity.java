4b3a84d33c50c672915edf547bde3e579b2bc2e5
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-11-08 21:14:00 +0700

    [ADD] Default and Signup Activity
    - Add interaction between activity

--- app/src/main/java/com/weouts/program/andro/weouts/FirstActivity.java
+++ app/src/main/java/com/weouts/program/andro/weouts/FirstActivity.java
@@ -1,13 +1,35 @@
 package com.weouts.program.andro.weouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private Button btnSigin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        btnSigin = (Button) findViewById(R.id.btn_signin);
+        btnSignup = (Button) findViewById(R.id.btn_signup);
+
+        btnSigin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FirstActivity.this, SigninActivity.class));
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FirstActivity.this, SignupActivity.class));
+            }
+        });
     }
 }
