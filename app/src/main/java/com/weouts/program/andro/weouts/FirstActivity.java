231f7ccaa4247b099226a6a32d46b87b4121a500
Andri Rahmat Herdiansyah <bandrotandri21@gmail.com>
2016-11-10 15:41:35 +0700

    [ADD] add button to navigation drawer fragment

--- app/src/main/java/com/weouts/program/andro/weouts/FirstActivity.java
+++ app/src/main/java/com/weouts/program/andro/weouts/FirstActivity.java
@@ -1,13 +1,40 @@
 package com.weouts.program.andro.weouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FirstActivity extends AppCompatActivity {
+    private Button btnSignin;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        btnSignin = (Button) findViewById(R.id.btn_signin);
+        btnSignup = (Button) findViewById(R.id.btn_signup);
+
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstActivity.this, SigninActivity.class));
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstActivity.this, SignupActivity.class));
+            }
+        });
+
+
     }
 }
