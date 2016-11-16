56c42bae86e0eb893546b3f4d97ef3a2a162b188
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-11-16 17:21:04 +0700

    [TEST] Activity to Fragment

--- app/src/main/java/com/weouts/program/andro/weouts/DashboardActivity.java
+++ app/src/main/java/com/weouts/program/andro/weouts/DashboardActivity.java
@@ -2,13 +2,19 @@ package com.weouts.program.andro.weouts;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -20,29 +26,33 @@ import java.util.List;
  * Created by mochadwi on 11/3/16.
  */
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends Fragment {
     private List<LocalSite> localSiteList = new ArrayList<>();
     private RecyclerView recyclerView;
     private LocalSiteAdapter localSiteAdapter;
+    private CoordinatorLayout coordinatorLayout;
+    private FragmentActivity fragmentActivity;
 
+    @Nullable
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        fragmentActivity = super.getActivity();
+        coordinatorLayout = (CoordinatorLayout) inflater.inflate(R.layout.activity_dashboard, container,
+                false);
 
-        recyclerView = (RecyclerView) findViewById(R.id.recv_dashboard);
+        recyclerView = (RecyclerView) coordinatorLayout.findViewById(R.id.recv_dashboard);
 
-        localSiteAdapter = new LocalSiteAdapter(this, localSiteList);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        localSiteAdapter = new LocalSiteAdapter(fragmentActivity, localSiteList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(fragmentActivity.getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(fragmentActivity.getApplicationContext(),
+                recyclerView, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
                 LocalSite localSite = localSiteList.get(position);
-                Toast.makeText(getApplicationContext(), localSite.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(fragmentActivity.getApplicationContext(),
+                        localSite.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -56,11 +66,51 @@ public class DashboardActivity extends AppCompatActivity {
 
         try {
             Glide.with(this).load(R.drawable.kawah_putih)
-                    .into((ImageView) findViewById(R.id.img_location));
+                    .into((ImageView) coordinatorLayout.findViewById(R.id.img_location));
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+        return coordinatorLayout;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    //    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        localSiteAdapter = new LocalSiteAdapter(this, localSiteList);
+//        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+//        recyclerView.setLayoutManager(mLayoutManager);
+//        recyclerView.setItemAnimator(new DefaultItemAnimator());
+//        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+//            @Override
+//            public void onClick(View view, int position) {
+//                LocalSite localSite = localSiteList.get(position);
+//                Toast.makeText(getApplicationContext(), localSite.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onLongClick(View view, int position) {
+//
+//            }
+//        }));
+//        recyclerView.setAdapter(localSiteAdapter);
+//
+//        prepareLocalSiteData();
+//
+//        try {
+//            Glide.with(this).load(R.drawable.kawah_putih)
+//                    .into((ImageView) findViewById(R.id.img_location));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     private void prepareLocalSiteData() {
         int[] thumbnail = new int[]{
